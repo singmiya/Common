76c8d9a1afede1a8733d1120050d5928f988e8af
singmiya <jjsomiya@gmail.com>
2018-06-14 14:31:28 +0800

    init

---
+++ Common/Common.h
@@ -0,0 +1,19 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by Somiya on 14/06/2018.
+//  Copyright © 2018 Somiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
